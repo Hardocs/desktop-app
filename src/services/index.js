12af974b2bd4a5636420956aae9b102d0a47a73e
Divine Hycenth <divinehycenth@outlook.com>
2020-09-02 12:03:54 +0100

    Added src/services/index.js.

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,13 @@
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import gql from "graphql-tag";
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 // import cwd from "./queries/cwd.gql"
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
-  uri: "http://localhost:4001/"
+  uri: 'http://localhost:4001/'
 });
 
 // Cache implementation
