c0232656021ba90f821bf81106abcd47100d381f
Jose Carlos Urra <jose.urra86@gmail.com>
2020-09-03 12:12:16 +0200

    Routing is working but contents are not updating

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="doc" v-if="doc">
-    <p>{{ id }}</p>
+    <p>{{ this.id }}</p>
     <p>{{ doc.title }}</p>
     <DocEditor :content="doc.content" :id="doc.id"></DocEditor>
   </div>
@@ -16,14 +16,30 @@ export default {
   components: { DocEditor },
   data() {
     return {
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      doc: {}
     };
   },
-  computed: {
-    doc() {
+
+  methods: {
+    getDoc() {
       // we use the id that is part of this object to
       // find the actual object stored in the vuex
-      return this.$store.state.docs.allDocs.find(doc => doc.id == this.id);
+      return (this.doc = this.$store.state.docs.allDocs.find(
+        doc => doc.id == this.id
+      ));
+    }
+  },
+  // mounted: function() {
+  //   console.log(this.doc);
+  //   this.doc = this.getDoc();
+  //   console.log("After")
+  //   console.log(this.doc);
+  // },
+  watch: {
+    $route: function() {
+      this.id = this.$route.params.id;
+      this.getDoc();
     }
   }
 };
