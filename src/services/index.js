2165e3f6c95687418b9e397078e61b2ef33e6072
Divine Hycenth <divinehycenth@outlook.com>
2020-09-25 11:55:17 +0100

    fix: ✌ delete file store function

--- src/services/index.js
+++ src/services/index.js
@@ -23,31 +23,32 @@ const CWD = gql`
 
 const CREATE_NEW_PROJECT = gql`
   mutation createProject($input: CreateProjectInput!) {
-  createProject(input: $input) {
-    path
-    name
-    shortTitle
-    allDocsData {
-      title
-      fileName
-      content
+    createProject(input: $input) {
+      path
+      name
+      shortTitle
+      allDocsData {
+        title
+        fileName
+        content
+      }
     }
   }
-}`;
+`;
 
 const CREATE_PROJECT_FROM_FOLDER = gql`
   mutation($input: CreateProjectInput!) {
-  createProjectFromExisting(input: $input) {
-    path
-    name
-    shortTitle
-    allDocsData {
-      title
-      fileName
-      content
+    createProjectFromExisting(input: $input) {
+      path
+      name
+      shortTitle
+      allDocsData {
+        title
+        fileName
+        content
+      }
     }
   }
-}
 `;
 
 const LOAD_DOCS = gql`
@@ -70,55 +71,53 @@ const LOAD_DOCS = gql`
 `;
 
 const WRITE_FILE = gql`
-mutation writeToFile($input: FileInput!) {
-  writeToFile(input: $input) {
-    path
+  mutation writeToFile($input: FileInput!) {
+    writeToFile(input: $input) {
+      path
+    }
   }
-}
-`
+`;
 
 const DELETE_FILE = gql`
-  mutation deleteFile($filePath: String!){
-  deleteFile(filePath: $filePath)
-}
-`
+  mutation deleteFile($filePath: String!) {
+    deleteFile(filePath: $filePath)
+  }
+`;
 
 export default {
-
   getCWD() {
     return apiClient.mutate({
       mutation: CWD
     });
   },
 
-/**
- * @param {Object} projectMetadata 
- */
+  /**
+   * @param {Object} projectMetadata
+   */
   createNewProject(projectMetadata) {
     return apiClient.mutate({
       mutation: CREATE_NEW_PROJECT,
       variables: {
         input: projectMetadata
       }
-    })
+    });
   },
 
-  
-/**
- * @param {Object} projectMetadata 
- */
+  /**
+   * @param {Object} projectMetadata
+   */
   createProjectFromFolder(projectMetadata) {
     return apiClient.mutate({
       mutation: CREATE_PROJECT_FROM_FOLDER,
       variables: {
         input: projectMetadata
       }
-    })
+    });
   },
 
-/**
- * @param {String} path 
- */
+  /**
+   * @param {String} path
+   */
   getProject(path) {
     return apiClient.mutate({
       mutation: LOAD_DOCS,
@@ -128,21 +127,21 @@ export default {
     });
   },
 
-  
- /** 
-   * @param {Object} fileMetadata 
-   */ 
+  /**
+   * @param {Object} fileMetadata
+   */
+
   writeFile(fileMetadata) {
     return apiClient.mutate({
       mutation: WRITE_FILE,
       variables: {
         input: fileMetadata
       }
-    })
+    });
   },
 
   /**
-   * @param {String} path 
+   * @param {String} path
    */
   deleteFile(path) {
     return apiClient.mutate({
@@ -150,6 +149,6 @@ export default {
       variables: {
         filePath: path
       }
-    })
+    });
   }
 };
--- src/store/docs.js
+++ src/store/docs.js
@@ -2,36 +2,35 @@
  * In HARDOCS, a project is composed of docs and metadata about the project.
  * this module stores all the specific state of project's documents.
  * This includes document files, folder holding the documents, the html contents,
- * and other data related to documents. 
+ * and other data related to documents.
  */
 
 import DocsServices from '@/services/index';
 import {
   // loadFilePathsFromSelectedFolder,
-  chooseFolderForUse,
+  chooseFolderForUse
   // putContentToSelectedFolder,
   // loadContentFromFilePath
-} from '@hardocs-project/habitat-client/lib/modules/habitat-localservices'
+} from '@hardocs-project/habitat-client/lib/modules/habitat-localservices';
 
 export const state = {
-  cwd: "",
-  docsFolder: "",
-  entryFile: "",
+  cwd: '',
+  docsFolder: '',
+  entryFile: '',
   devFeatures: process.env.devFeatures,
   allDocs: [],
   currentDoc: {}
 };
 
-const defaultNewDocName = "Untitled"
+const defaultNewDocName = 'Untitled';
 
 export const mutations = {
-
   SET_CWD(state, cwd) {
     state.cwd = cwd;
   },
 
-  SET_ENTRY_FILE(state, entryFile){
-    state.entryFile = entryFile
+  SET_ENTRY_FILE(state, entryFile) {
+    state.entryFile = entryFile;
   },
 
   SET_DOCS_FOLDER(state, docsFolder) {
@@ -42,8 +41,9 @@ export const mutations = {
     state.allDocs = allDocs;
     if (allDocs) {
       state.currentDoc = allDocs[0];
+    } else {
+      state.currentDoc = undefined;
     }
-    else { state.currentDoc = undefined }
   },
 
   ADD_DOC(state, doc) {
@@ -65,7 +65,7 @@ export const mutations = {
   },
 
   SET_TO_UNSAVED(state) {
-    state.currentDoc.saved = false
+    state.currentDoc.saved = false;
   },
 
   UPDATE_DOC_CONTENT(state, editedDoc) {
@@ -76,19 +76,23 @@ export const mutations = {
 };
 
 export const actions = {
-  openFolder: function () {
-    return chooseFolderForUse()
+  openFolder: function() {
+    return chooseFolderForUse();
   },
 
   async loadProject({ commit, state }) {
-    const cwd = await chooseFolderForUse()
-    if(cwd !== state.cwd){
+    const cwd = await chooseFolderForUse();
+    if (cwd !== state.cwd) {
       const response = await DocsServices.getProject(cwd);
-      const formattedDocs = formatDocs(response, 'openProject', state.entryFile)
+      const formattedDocs = formatDocs(
+        response,
+        'openProject',
+        state.entryFile
+      );
       commit('SET_CWD', cwd);
       commit('LOAD_DOCS', formattedDocs);
-      commit('SET_DOCS_FOLDER', response.data.openProject.docsDir)
-      commit('SET_ENTRY_FILE', response.data.openProject.entryFile )
+      commit('SET_DOCS_FOLDER', response.data.openProject.docsDir);
+      commit('SET_ENTRY_FILE', response.data.openProject.entryFile);
     }
   },
 
@@ -98,8 +102,7 @@ export const actions = {
       if (doc) {
         commit('SET_CURRENT_DOC', doc);
       }
-    }
-    else {
+    } else {
       const doc = this.state.docs.allDocs[index];
       commit('SET_CURRENT_DOC', doc);
     }
@@ -111,37 +114,37 @@ export const actions = {
       const doc = {
         id: newId,
         title: defaultNewDocName,
-        content: "Edit new document",
-        description: "Edit this doc",
+        content: 'Edit new document',
+        description: 'Edit this doc',
         saved: false
-      }
+      };
 
       if (doc.fileName == state.entryFile) {
         doc.fileName = state.entryFile;
-      }
-
-      else {
+      } else {
         // Make sure that there are no duplicate titles
-        for (var i = 0; i < state.allDocs.length; i++) { // FIXME: Generalize this for different scenarios
+        for (var i = 0; i < state.allDocs.length; i++) {
+          // FIXME: Generalize this for different scenarios
           if (state.allDocs[i].title == doc.title) {
-            doc.title = doc.title + " copy";
-            doc.content = doc.title
+            doc.title = doc.title + ' copy';
+            doc.content = doc.title;
           }
         }
-        doc['fileName'] = `${doc.title.split(' ').join('-')}` // FIXME: check for duplicates
+        doc['fileName'] = `${doc.title.split(' ').join('-')}`; // FIXME: check for duplicates
       }
-      return doc
+      return doc;
     }
-    const doc = await makeDoc()
-    await dispatch('writeFileRequest', doc)
-      .catch((err) => { console.log(err) })
-    await commit('ADD_DOC', doc)
-    commit('SET_TO_SAVED', doc.id)
-
+    const doc = await makeDoc();
+    await dispatch('writeFileRequest', doc).catch((err) => {
+      console.log(err);
+    });
+    await commit('ADD_DOC', doc);
+    commit('SET_TO_SAVED', doc.id);
   },
 
   async writeFileRequest({ state, commit }, newDoc) {
-    console.log(commit)
+    console.log(commit);
+
     function makeReq(newDoc) {
       return {
         title: newDoc.title,
@@ -149,51 +152,57 @@ export const actions = {
         path: state.docsFolder,
         fileName: `/${newDoc.fileName}.md`,
         content: newDoc.content
-      }
+      };
     }
-    const req = await makeReq(newDoc)
-    await DocsServices.writeFile(req)
+    const req = await makeReq(newDoc);
+    await DocsServices.writeFile(req);
   },
 
-  deleteDocFile(path) {
-    DocsServices.deleteFile(path)
-  },
-
-  async saveDocFile({ state, dispatch }) { //FIXME: The contract is not working
-    const newDoc = await state.currentDoc
+  async saveDocFile({ state, dispatch }) {
+    //FIXME: The contract is not working
+    const newDoc = await state.currentDoc;
     // if (state.currentDoc.saved == false) {
-      console.log(`Saving a file ${state.cwd}/${state.docsFolder}/${newDoc.fileName}`)
-      await DocsServices.deleteFile(`${state.cwd}/${state.docsFolder}/${newDoc.fileName}.md`)
-      newDoc['fileName'] = `${newDoc.title.split(' ').join('-')}`
-      dispatch('writeFileRequest', newDoc)
+    console.log(
+      `Saving a file ${state.cwd}/${state.docsFolder}/${newDoc.fileName}`
+    );
+    await DocsServices.deleteFile(
+      `${state.cwd}/${state.docsFolder}/${newDoc.fileName}.md`
+    );
+    newDoc['fileName'] = `${newDoc.title.split(' ').join('-')}`;
+    dispatch('writeFileRequest', newDoc);
     // }
   },
 
   async removeDoc({ state, commit }, id) {
     const newDoc = state.allDocs.find((doc) => doc.id == id);
+
+    const filePath = `${state.docsFolder}/${newDoc.fileName}`;
+
     if (newDoc.fileName !== state.entryFile) {
-      console.log(`${state.cwd}/${state.docsFolder}/${newDoc.fileName}`) // FIXME:There is inconsistency with the extensions like .md
-      await DocsServices.deleteFile(`${state.cwd}/${state.docsFolder}/${newDoc.fileName}.md`)
-      commit('REMOVE_DOC', id)
+      console.log({ filePath }); // FIXME:There is inconsistency with the extensions like .md
+      await DocsServices.deleteFile(filePath);
+      commit('REMOVE_DOC', id);
     }
   },
 
   async createNewProject({ commit }, projectMetadata) {
-    const response = await DocsServices.createNewProject(projectMetadata)
-    const result = formatDocs(response, 'createProject')
-    commit('LOAD_DOCS', result)
+    const response = await DocsServices.createNewProject(projectMetadata);
+    const result = formatDocs(response, 'createProject');
+    commit('LOAD_DOCS', result);
   },
 
   async createProjectFromFolder({ commit }, projectMetadata) {
-    const response = await DocsServices.createProjectFromFolder(projectMetadata)
-    const result = formatDocs(response, 'createProjectFromExisting')
-    commit('LOAD_DOCS', result)
-  },
+    const response = await DocsServices.createProjectFromFolder(
+      projectMetadata
+    );
+    const result = formatDocs(response, 'createProjectFromExisting');
+    commit('LOAD_DOCS', result);
+  }
 };
 
- function formatDocs(response, gqlAction) {
+function formatDocs(response, gqlAction) {
   //Check if mutation exists or not
-  console.log(response.data[gqlAction])
+  console.log(response.data[gqlAction]);
   response.data[gqlAction].allDocsData.filter(async (element) => {
     // create id
     element.id = Math.floor(Math.random() * 1000000);
@@ -202,13 +211,14 @@ export const actions = {
     let regex = /<[^>].+?>(.*?)<\/.+?>/m;
     if (element.content.match(regex)) {
       element.title = await element.content.match(regex)[0];
+    } else {
+      element.title = element.content;
     }
-    else { element.title = element.content }
 
     // Step 2: get only text inside h1 tags
     regex = /(<([^>]+)>)/gi;
     element.title = await element.title.replace(regex, '').trim();
     element.saved = true;
   });
-  return response.data[gqlAction].allDocsData
+  return response.data[gqlAction].allDocsData;
 }
