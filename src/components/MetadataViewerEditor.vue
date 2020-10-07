fef7bdf51dfc51bfc9c28833f5ae8acefdbefa63
Jose Carlos Urra <jose.urra86@gmail.com>
2020-10-07 15:00:59 +0200

    Created the component

---
+++ src/components/MetadataViewerEditor.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <JsonEditor :is-edit="true" v-model="jsonData"></JsonEditor>
+    </div>
+</template>
+<script>
+import JsonEditor from 'vue-edit-json'
+export default {
+    name: 'MetadataViewerEditor',
+    components: { JsonEditor }
+}
+</script>
