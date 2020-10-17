42a069b40b860d43238047c8c2934445936ab20c
Jose Carlos Urra <jose.urra86@gmail.com>
2020-10-17 13:37:28 +0200

    Layout for the metadata side panel

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -10,22 +10,27 @@
         <DocsContents></DocsContents>
       </aside>
       <div class="w-1/5"></div>
-      <div class="w-3/4 xl:w-2/4">
+      <div class="w-3/4 xl:w-2/4 pl-4">
         <slot />
       </div>
-      <div :class="{active: metadata}" class="fixed pt-10 top-0 right-0 h-screen flex">
+      <div
+        :class="{ active: metadata }"
+        class="fixed pt-10 top-0 right-0 h-screen flex "
+      >
         <div class="flex items-center transform -rotate-90 w-4">
           <div class="bg-primary-100 text-white">
-            <p 
-            class="pl-3 pr-3" style="cursor:pointer;" @click="toggleOpen()">METADATA</p>
+            <p class="pl-3 pr-3" style="cursor:pointer;" @click="toggleOpen()">
+              METADATA
+            </p>
           </div>
         </div>
         <div class="bg-primary-100 w-3"></div>
         <div
-          :class="{hidden: !metadata}"
-          class="bg-gray-15 w-full pr-4 border-r border-solid border-gray-25"
+          id="app"
+          :class="{ hidden: !metadata }"
+          class="bg-gray-15 w-full pr-4 border-r border-solid border-gray-25 overflow-auto"
         >
-          <div>
+          <div class="">
             <MetadataPanel></MetadataPanel>
           </div>
         </div>
@@ -50,22 +55,16 @@ export default {
       metadata: false
     };
   },
-  methods:{
+  methods: {
     toggleOpen() {
       this.metadata = !this.metadata;
-    },
+    }
   }
 };
 </script>
 <style scoped>
-.side-panel__btn {
-  display: flex;
-  background-color: blue;
-  color: white;
-  text-align: center;
-}
 
-.active{
+.active {
   @apply w-1/3;
 }
 </style>
