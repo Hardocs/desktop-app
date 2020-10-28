065352af09dd5bee7493172ec53de10c40dd0f31
Divine Hycenth <49137104+DNature@users.noreply.github.com>
2020-10-28 12:10:23 +0100

    Feature/icons (#23)
    * Revert "Fixed wrong statement of reactive state with proper computed properties"
    
    This reverts commit 3ec2214e75c5c3182fd8f1fea1961871f4701ca0.
    
    * Fixed wrong statement of reactive state with proper computed properties
    
    * - Turned mutation into an action
    - Added getter for the saveFile feature
    - Assigned properly a new key to the object
    
    * Server usage back to where it was working
    
    * adding icons
    
    * added icons
    
    * added feather icon
    
    * example handling view with simple css
    
    * translated icons up
    
    * fix: ✌ styled editor properly
    
    Co-authored-by: Jose Carlos Urra <jose.urra86@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,12 +1,5 @@
-// const http = require('http');
-// console.log({ Hello: 'Hello world from preload file (src/server.js)' });
+console.log({ Hello: 'Hello world from preload file (src/server.js)' });
 const { server } = require('hardocs-graphql-api');
-server();
-// create a server object:
 
-// http
-//   .createServer(function(req, res) {
-//     res.write('Hello World!'); //write a response to the client
-//     res.end(); //end the response
-//   })
-//   .listen(5001, () => console.log('Listening on 5001')); //the server object listens on port 8080
+//create a server object:
+server();
