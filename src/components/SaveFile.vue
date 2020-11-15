75c02449259c68b6202e70be45bd9a7575f66f54
Jose Carlos Urra <jose.urra86@gmail.com>
2020-11-15 12:21:33 +0100

    Merge branch 'feature/issue-29-handle-base-exceptions' into develop

--- src/components/SaveFile.vue
+++ src/components/SaveFile.vue
@@ -25,7 +25,7 @@ export default {
       return this.$store.state.docs.currentDoc.saved
     },
     guidesIsActive(){
-      return this.$store.state.docs.guidesIsActive
+      return this.$store.getters.guidesIsActive
     }
   },
   methods: {
