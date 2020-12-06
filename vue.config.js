385c2b5f261e8b286846b2a4461505553a09b724
Divine Hycenth <divinehycenth@outlook.com>
2020-12-06 20:49:50 +0100

    created snap release

--- vue.config.js
+++ vue.config.js
@@ -18,30 +18,42 @@ module.exports = {
   },
   pluginOptions: {
     electronBuilder: {
-      // preload: 'src/server.js',
       nodeIntegration: true,
-      linux: {
-        target: 'nsis',
-        category: 'System'
-      },
-      win: {
-        target: 'deb',
-        category: 'System'
-      },
+      // preload: 'src/server.js',
+
       builderOptions: {
+        appId: 'hardocs.github.io',
+        productName: 'Hardocs Desktop',
+        // copyright: '',
+        linux: {
+          target: 'snap',
+          category: 'System',
+          icon: 'src/assets/logo.png'
+        },
+        // win: {
+        //   target: 'nsis',
+        //   oneClick: true,
+        //   category: 'System',
+        //   installerIcon: 'src/assets/favicon.ico',
+        //   uninstallerIcon: 'src/assets/favicon.ico',
+        //   shortcutName: 'Hardocs'
+        // },
+        mac: {
+          category: 'public.app-category.utilities',
+          target: 'dmg'
+          // type: 'dis'
+        },
+        publish: ['github'],
         // options placed here will be merged with default configuration and passed to electron-builder
-        files: [
-          "**/*"
-        ],
+        files: ['**/*'],
         extraFiles: [
           {
-              "from": "guides",
-              "to": "guides",
-              "filter": ["**/*"]
+            from: 'guides',
+            to: 'guides',
+            filter: ['**/*']
           }
         ]
       }
-    },
-    
+    }
   }
 };
