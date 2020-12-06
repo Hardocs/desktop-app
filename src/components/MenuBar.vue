f3d862a2f91e4d20df6e9762c3f973f30ed6fb18
jurra <jose.urra86@gmail.com>
2020-12-06 11:44:24 +0100

    separated divs for project folder templating so that the user can easily select the path at the top of the menubar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -14,29 +14,33 @@
       class="border-solid border-b border-gray-25 py-4 w-full flex border-b-1"
     >
       <div v-for="item in actions" :key="item.actionType" class="px-4">
-        <p 
-        style="cursor:pointer"
-        @click="openHardocsPath(item.actionType, item.initOn)">
+        <p
+          style="cursor: pointer"
+          @click="openHardocsPath(item.actionType, item.initOn)"
+        >
           {{ item.label }}
         </p>
       </div>
       <!-- <StandardSelector></StandardSelector> -->
       <div class="w-1/2">
-      <p class="text-center"><strong>Project Folder: </strong>{{ cwd }}</p>
+        <div class="text-center flex justify-center">
+          <strong>Project Folder: </strong>
+          <div class="px-4">{{ cwd }}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 // import here the modal component...
-import CreateProject from '@/components/CreateProject'
+import CreateProject from '@/components/CreateProject';
 // import StandardSelector from '@/components/MetadataEdit__SchemasDir';
 
 export default {
   name: 'MenuBar',
-  components: { 
-    CreateProject, 
-    // StandardSelector 
+  components: {
+    CreateProject
+    // StandardSelector
   },
   data() {
     return {
@@ -52,7 +56,7 @@ export default {
           label: 'Open project',
           actionType: 'loadProject',
           initOn: false
-        },
+        }
         // {
         //   label: 'Create from folder',
         //   actionType: 'createProjectFromExisting',
@@ -61,14 +65,14 @@ export default {
       ]
     };
   },
- 
+
   computed: {
     isInit() {
       // Compute if initialization is taking place or not
       return this.$store.state.docs.initProject.on;
     },
-    cwd(){
-      return this.$store.state.docs.cwd
+    cwd() {
+      return this.$store.state.docs.cwd;
     }
   },
   methods: {
