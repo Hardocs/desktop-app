0cd90afb60530b92bcf3f9ee39f4fce110690bdc
Divine Hycenth <divinehycenth@outlook.com>
2020-12-08 13:02:55 +0100

    added publish option

--- vue.config.js
+++ vue.config.js
@@ -28,22 +28,24 @@ module.exports = {
         linux: {
           target: 'snap',
           category: 'System',
-          icon: 'src/assets/logo.png'
+          icon: 'src/assets/logo.png',
+          publish: ['github']
         },
-        // win: {
-        //   target: 'nsis',
-        //   oneClick: true,
-        //   category: 'System',
-        //   installerIcon: 'src/assets/favicon.ico',
-        //   uninstallerIcon: 'src/assets/favicon.ico',
-        //   shortcutName: 'Hardocs'
-        // },
+        win: {
+          target: 'nsis',
+          oneClick: true,
+          category: 'System',
+          installerIcon: 'src/assets/favicon.ico',
+          uninstallerIcon: 'src/assets/favicon.ico',
+          shortcutName: 'Hardocs',
+          publish: ['github']
+        },
         mac: {
           category: 'public.app-category.utilities',
-          target: 'dmg'
+          target: 'dmg',
+          publish: ['github']
           // type: 'dis'
         },
-        publish: ['github'],
         // options placed here will be merged with default configuration and passed to electron-builder
         files: ['**/*'],
         extraFiles: [
