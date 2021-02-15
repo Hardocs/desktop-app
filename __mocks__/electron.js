9e595b9d7818f63984950e8578d315073732b788
Divine Hycenth <divinehycenth@outlook.com>
2021-02-15 10:16:31 +0100

    linting fixes

---
+++ __mocks__/electron.js
@@ -0,0 +1,12 @@
+module.exports = {
+  require: jest.fn(),
+  match: jest.fn(),
+  app: jest.fn(),
+  remote: {
+    app: {
+      getPath: jest.fn()
+    },
+    getCurrentWindow: jest.fn()
+  },
+  dialog: jest.fn()
+};
