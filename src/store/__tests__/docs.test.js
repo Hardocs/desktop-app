5e7359b02448694555c48f40eca155860c7ee484
Divine Hycenth <divinehycenth@outlook.com>
2021-02-16 14:58:15 +0100

    feat: 🍞 added create hardocs project test

--- src/store/__tests__/docs.test.js
+++ src/store/__tests__/docs.test.js
@@ -10,6 +10,7 @@ import { createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import store from '..';
 import { types as mutations, actions } from '../docs';
+import fs from 'fs';
 
 import { resetState, DEFAULT_STATE } from '../helpers/resetState';
 
@@ -23,7 +24,26 @@ describe('Test for docs operations', () => {
     expect(store.state).toStrictEqual(DEFAULT_STATE);
   });
 
-  test('opens a hardocs project and update the state', async () => {
+  test('Creates a hardocs project and updates the store', async () => {
+    /**  Disable console log */
+    console.log = () => {};
+    const name = 'test-project';
+
+    await store.dispatch('createNewProject', {
+      docsDir: 'docs',
+      entryFile: 'index.html',
+      name: name,
+      path: process.cwd(),
+      shortTitle: ''
+    });
+
+    await store.dispatch('loadProject');
+
+    expect(store.state).not.toStrictEqual(DEFAULT_STATE);
+    expect(store.state.docs.allDocs).not.toStrictEqual([]);
+  });
+
+  test('opens a hardocs project and updates the store', async () => {
     /** allDocs in state is empty */
     expect(store.state.docs.allDocs).toEqual([]);
 
