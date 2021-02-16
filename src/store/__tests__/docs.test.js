bcdf08e64a4c7c36f7354496f48aaf2464bf4a2c
Divine Hycenth <divinehycenth@outlook.com>
2021-02-16 15:41:03 +0100

    feat: 🍞 added tests for creating and deleting documents

--- src/store/__tests__/docs.test.js
+++ src/store/__tests__/docs.test.js
@@ -10,8 +10,6 @@ import { createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import store from '..';
 import { types as mutations, actions } from '../docs';
-import fs from 'fs';
-
 import { resetState, DEFAULT_STATE } from '../helpers/resetState';
 
 const localVue = createLocalVue();
@@ -26,7 +24,6 @@ describe('Test for docs operations', () => {
 
   test('Creates a hardocs project and updates the store', async () => {
     /**  Disable console log */
-    console.log = (...params) => params;
     const name = 'test-project';
 
     await store.dispatch('createNewProject', {
@@ -55,5 +52,29 @@ describe('Test for docs operations', () => {
 
     /** allDocs in state is no longer empty */
     expect(store.state.docs.allDocs).not.toEqual([]);
+  });
+
+  test('Saves a document locally and to the store', async () => {
+    expect(store.state.docs.allDocs).toStrictEqual([]);
+
+    store.commit(mutations.SET_CWD, actions.cwd().data.cwd); // The `cwd` is already set to our test-project path
+    await store.dispatch('loadProject');
+    await store.dispatch('addDoc');
+    await store.dispatch('saveDocFile');
+
+    expect(store.state.docs.allDocs.length).toBe(2); // I still haven't figured a way to handle this
+  });
+
+  /**
+   * This test depends on the previous test to check if the length of allDocs is "2"
+   */
+  test('Delete a document locally updates the store', async () => {
+    expect(store.state.docs.allDocs).toStrictEqual([]);
+
+    store.commit(mutations.SET_CWD, actions.cwd().data.cwd); // The `cwd` is already set to our test-project path
+    await store.dispatch('loadProject');
+    // await store.dispatch('setCurrentDoc', '2');
+    await store.dispatch('removeDoc', '2');
+    expect(store.state.docs.allDocs.length).not.toBe(2); // I still haven't figured a way to handle this
   });
 });
