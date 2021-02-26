582bad453d6506d77972fa87d063530a2214f826
Divine Hycenth <divinehycenth@outlook.com>
2021-02-26 12:01:46 +0100

    feat: ✅ cleaned up console logs

--- src/services/ipc-operations.js
+++ src/services/ipc-operations.js
@@ -2,9 +2,7 @@ import { ipcRenderer } from 'electron';
 
 function checkUnsavedDocs(store) {
   return ipcRenderer.on('checkUnsavedDocs', () => {
-    console.log('Getting value from vuex getter to the main process');
     let response = store.getters.hasUnsavedFiles > 0;
-    console.log('Response coming from vuex: ' + response);
     ipcRenderer.send('hasUnsavedFiles', response);
   });
 }
@@ -14,11 +12,9 @@ function checkUnsavedDocs(store) {
 
 function passAppPath(store) {
   return ipcRenderer.on('passAppPath', async (event, path) => {
-    console.log('Path coming from background process: ' + path);
     await store.commit('SET_APP_PATH', path);
     await store.commit('SET_CWD', path);
     await store.dispatch('loadProject');
-    console.log(store.state.docs.cwd);
   });
 }
 
