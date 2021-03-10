19df299ce684612af8ab3972f9e5f8a3f946b325
Divine Hycenth <divinehycenth@outlook.com>
2021-03-10 09:52:51 +0100

    removed tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 // postcss.config.js
 
 const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
 
 module.exports = {
   // eslint-disable-next-line prettier/prettier
-    plugins: [
-        tailwindcss,
-        autoprefixer,
-    ],
+  plugins: [autoprefixer]
 };
