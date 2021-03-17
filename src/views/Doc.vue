0e2429cbf2a001e69659c6893ae32a5aa5b79b7a
Divine Hycenth <divinehycenth@outlook.com>
2021-03-17 11:34:21 +0100

    feat: 🍞 implemented sticky editor/preview menubar

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -5,50 +5,55 @@
       <p>{{ this.$store.state.docs.currentDoc.title }}</p>
     </div>
 
-    <v-container class="d-flex justify-space-between">
-      <SaveFile :isSaved="docIsSaved" :docId="docId"> </SaveFile>
-      <span>
-        <v-btn
-          @click="editMode = false"
-          :color="!editMode ? 'primary' : ''"
-          class="mr-3"
-          outlined
-          rounded
-        >
-          Preview
-        </v-btn>
-        <v-btn
-          :elevation="editMode ? 5 : 2"
-          rounded
-          icon
-          @click="editMode = true"
-          :color="editMode ? 'primary' : ''"
-          class="mr-3"
-        >
-          <v-icon>mdi-pencil</v-icon>
-        </v-btn>
-        <v-btn
-          elevation="2"
-          rounded
-          icon
-          @click="confirmDelete(id)"
-          class="mr-3"
-          :disabled="isEntry"
-        >
-          <v-icon>mdi-trash-can-outline</v-icon>
-        </v-btn>
-      </span>
-    </v-container>
+    <div class="editor_menubar">
+      <v-container class="d-flex justify-space-between">
+        <SaveFile :isSaved="docIsSaved" :docId="docId"> </SaveFile>
+        <span>
+          <v-btn
+            @click="editMode = false"
+            :color="!editMode ? 'primary' : ''"
+            class="mr-3"
+            outlined
+            rounded
+          >
+            Preview
+          </v-btn>
+          <v-btn
+            :elevation="editMode ? 5 : 2"
+            rounded
+            icon
+            @click="editMode = true"
+            :color="editMode ? 'primary' : ''"
+            class="mr-3"
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+          <v-btn
+            elevation="2"
+            rounded
+            icon
+            @click="confirmDelete(id)"
+            class="mr-3"
+            :disabled="isEntry"
+          >
+            <v-icon>mdi-trash-can-outline</v-icon>
+          </v-btn>
+        </span>
+      </v-container>
 
-    <v-divider class="mb-4"></v-divider>
+      <v-divider class="mb-4"></v-divider>
+    </div>
 
     <div v-html="docContent" v-if="!editMode" class="px-8 py-8"></div>
-    <DocEditor
-      :content="docContent"
-      :id="id"
-      v-if="editMode"
-      :key="componentKey"
-    ></DocEditor>
+    <div class="editor_container">
+      <DocEditor
+        :content="docContent"
+        class="ckeditor__"
+        :id="id"
+        v-if="editMode"
+        :key="componentKey"
+      ></DocEditor>
+    </div>
   </v-card>
   <div v-else>
     <p>No doc in this route</p>
@@ -151,3 +156,24 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.editor_menubar {
+  position: sticky;
+  top: 4.2rem;
+  background-color: white;
+}
+
+.editor_container {
+  max-height: 70vh !important;
+  overflow-y: scroll;
+}
+
+.editor_container {
+  scrollbar-width: none !important;
+}
+
+.editor_container::-webkit-scrollbar {
+  display: none !important;
+}
+</style>
