ed5945dc703a2820757a6525105ddd0a3209d17f
Divine Hycenth <divinehycenth@outlook.com>
2021-03-28 14:15:16 +0100

    refactored and stored schema in the state

---
+++ src/services/metadata.js
@@ -0,0 +1,19 @@
+import { habitatLocal } from '@hardocs-project/habitat-client';
+// import { project } from 'hardocs-fs';
+
+export default {
+  loadSchemaToState: async () => {
+    try {
+      const data = await habitatLocal.selectContentFromFolder(
+        ['json'],
+        'Json file'
+      );
+
+      const response = data.content;
+      // await project.updateSchema({content: response}) // TODO: Implement this in the hardocs-fs module
+      return response;
+    } catch (err) {
+      console.error('initProject:err: ' + err);
+    }
+  }
+};
