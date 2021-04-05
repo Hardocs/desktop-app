e46a779553415cfb2b81735d6a4da8f2656e5c61
Divine Hycenth <divinehycenth@outlook.com>
2021-04-05 08:26:08 +0100

    code splitting for docs state

---
+++ src/store/docs/index.js
@@ -0,0 +1,6 @@
+import { actions, state } from './actions';
+import { getters } from './getters';
+import { mutations } from './mutations';
+import { types } from './types';
+
+export { getters, actions, mutations, types, state };
