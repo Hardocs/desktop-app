a1f547f003ebc44f397023bef54d1d41d2aa660a
Divine Hycenth <divinehycenth@outlook.com>
2021-04-06 18:10:42 +0100

    refactored vjsf style and script imports

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import vuetify from '@/plugins/vuetify';
 import CKEditor from '@ckeditor/ckeditor5-vue';
-import './assets/styles/index.css';
+import '@koumoul/vjsf/lib/deps/third-party.js';
+import '@koumoul/vjsf/lib/VJsf.css';
+import Vue from 'vue';
 import JsonEditor from 'vue-json-edit';
 import vueShortkey from 'vue-shortkey';
-import vuetify from '@/plugins/vuetify';
+import App from './App.vue';
+import './assets/styles/index.css';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
