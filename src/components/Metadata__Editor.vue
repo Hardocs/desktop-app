6de2cc59ce5388975909b7dbd065bf4639f807ed
Divine Hycenth <divinehycenth@outlook.com>
2021-04-06 20:51:39 +0100

    trying to display metadata form from schema

---
+++ src/components/Metadata__Editor.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card>
+    <div style="cursor:pointer" class="float-right px-4" @click="cancel">
+      ❌
+    </div>
+    <v-card-title class="headline lighten-2">
+      Create a new Project
+    </v-card-title>
+    <v-card-text>
+      <v-form v-model="valid">
+        <v-jsf
+          v-model="model"
+          :schema="schema"
+          @submit.prevent
+          ref="formSchema"
+        >
+        </v-jsf>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn class="primary" @click="onSubmit()">Create project</v-btn>
+    </v-card-actions>
+    <v-snackbar v-model="requiredProps.length">
+      The following fields are required:
+      <strong>{{ requiredProps.join(', ') }}</strong>
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          small
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="requiredProps = []"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-card>
+</template>
+
+<script>
+/** Vjsf */
+import VJsf from '@koumoul/vjsf';
+import '@koumoul/vjsf/lib/VJsf.css';
+import '@koumoul/vjsf/lib/deps/third-party.js';
+
+export default {
+  name: 'MetadataEditor',
+  components: {
+    VJsf
+  },
+  data: function() {
+    return {
+      created: false,
+      schema: this.$store.state.docs.schema,
+      model: {},
+      requiredProps: [],
+      valid: false
+    };
+  },
+  mounted() {
+    // this.schema = this.$store.state.docs.schema;
+    // console.log(this.schema);
+  },
+  methods: {
+    onSubmit() {},
+    cancel() {
+      this.$store.commit('SET_INIT_PROJECT', {
+        on: false,
+        type: undefined
+      });
+    }
+  }
+};
+</script>
