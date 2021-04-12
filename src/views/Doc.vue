e395aafeed9915a667c9fa796b2e31e38c1544b1
Divine Hycenth <divinehycenth@outlook.com>
2021-04-12 18:05:55 +0100

    feature: update metadata in state and file system

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <DocContent />
-    <!-- <MetadataEditor /> -->
+    <MetadataEditor />
   </div>
 </template>
 
 <script>
 import DocContent from '@/components/Doc';
-// import MetadataEditor from '@/components/Metadata__Editor';
+import MetadataEditor from '@/components/Metadata__Editor';
 
 export default {
-  components: { DocContent }
-  // components: { DocContent, MetadataEditor }
+  // components: { DocContent }
+  components: { DocContent, MetadataEditor }
 };
 </script>
