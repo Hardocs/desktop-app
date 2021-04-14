b4f9d2f697bfa52630927c95c480168ac52aa03a
Divine Hycenth <divinehycenth@outlook.com>
2021-04-14 10:12:26 +0100

    implemented editing and previewing of metadata

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
     <DocContent />
-    <MetadataEditor />
   </div>
 </template>
 
 <script>
 import DocContent from '@/components/Doc';
-import MetadataEditor from '@/components/Metadata__Editor';
 
 export default {
-  // components: { DocContent }
-  components: { DocContent, MetadataEditor }
+  components: { DocContent }
 };
 </script>
