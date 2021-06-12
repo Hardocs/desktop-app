06e8eddc064ce0a18da136406219dac33ca04f75
Divine Hycenth <divinehycenth@outlook.com>
2021-06-12 21:27:33 +0100

    docs.tests are passing correctly

--- src/components/Doc__Sidebar.vue
+++ src/components/Doc__Sidebar.vue
@@ -56,18 +56,21 @@
         <v-card>
           <v-container>
             <v-form>
-              <v-text-field
-                v-model="schemaUrl"
-                label="Schema URL"
-              ></v-text-field>
-              <v-text-field
-                v-model="schemaName"
-                aria-required="true"
-                label="Schema Name"
-              ></v-text-field>
-              <v-btn @click="loadSchema">Create</v-btn>
+              <v-jsf
+                v-model="model"
+                :schema="schema"
+                @submit.prevent
+                ref="formSchema"
+              >
+              </v-jsf>
             </v-form>
           </v-container>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn @click="addMetadata" color="primary">Create record</v-btn>
+            <v-btn @click="open = false">Close</v-btn>
+          </v-card-actions>
         </v-card>
       </template>
     </v-dialog>
@@ -76,9 +79,13 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import VJsf from '@koumoul/vjsf';
 
 export default {
   name: 'DocsSidebar',
+  components: {
+    VJsf
+  },
   data() {
     return {
       showModal: false,
@@ -87,8 +94,29 @@ export default {
       currentDoc: '',
       mini: true,
       open: false,
-      schemaUrl: '',
-      schemaName: '',
+      schema: {
+        type: 'object',
+        title: 'Create record',
+        required: ['title', 'schemaUrl'],
+        properties: {
+          title: {
+            type: 'string',
+            title: 'Record name',
+            default: ''
+          },
+          schemaTitle: {
+            type: 'string',
+            title: 'Schema title',
+            default: ''
+          },
+          schemaUrl: {
+            type: 'string',
+            title: 'Schema URL',
+            default: ''
+          }
+        }
+      },
+      model: {},
       docType: ['Metadata', 'Document']
     };
   },
@@ -108,9 +136,6 @@ export default {
     },
     docs() {
       return this.$store.state.docs.hardocs;
-    },
-    entryFile() {
-      return this.$store.state.docs.entryFile;
     },
     cwd: {
       get() {
@@ -146,12 +171,9 @@ export default {
       }
     },
 
-    async loadSchema() {
+    async addMetadata() {
       this.$store
-        .dispatch('addMetadata', {
-          url: this.schemaUrl,
-          label: this.schemaName
-        })
+        .dispatch('addMetadata', this.model)
         .then(() => {
           this.open = false;
         })
